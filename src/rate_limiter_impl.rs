378bc5ad2b2bf2abfbcbf42236d75a434fd5de4d
ehsan <ehsan2003.2003.382@gmail.com>
2022-05-28 12:06:57 +0430

    adds garbage collection for rate_limiter_impl

--- src/rate_limiter_impl.rs
+++ src/rate_limiter_impl.rs
@@ -1,4 +1,9 @@
-use std::{time::{Duration, Instant}, collections::HashMap, sync::RwLock, hash::Hash};
+use std::{
+    collections::HashMap,
+    hash::Hash,
+    sync::RwLock,
+    time::{Duration, Instant},
+};
 
 use crate::rate_limiter::RateLimiter;
 
@@ -12,6 +17,27 @@ impl<T: Hash + Eq + Send + Sync> RateLimiterImpl<T> {
         Self {
             list: (RwLock::new(Default::default())),
             duration: d,
+        }
+    }
+    pub async fn run_garbage_collector(
+        &self,
+        interval: Duration,
+        mut shutdown_rx: tokio::sync::broadcast::Receiver<()>,
+    ) {
+        let mut interval = tokio::time::interval(interval);
+        while let Some(now) = tokio::select! {
+            now = interval.tick() => Some(now),
+            _ = shutdown_rx.recv() => None,
+
+        } {
+            {
+                let mut lock = self.list.write().unwrap();
+                let now = now.into_std();
+                *lock = lock
+                    .drain()
+                    .filter(|(_, v)| v < &now)
+                    .collect::<HashMap<T, Instant>>();
+            };
         }
     }
 }
@@ -36,7 +62,7 @@ impl<T: Hash + Eq + Send + Sync> RateLimiter<T> for RateLimiterImpl<T> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::time::Duration;
+    use std::{sync::Arc, time::Duration};
     const DURATION: Duration = Duration::from_millis(100);
     #[fixture]
     fn limiter() -> RateLimiterImpl<u32> {
@@ -77,5 +103,21 @@ mod tests {
     async fn it_should_work_with_different_keys(limiter: RateLimiterImpl<u32>) {
         limiter.mark_as_limited(1).await;
         assert!(limiter.is_free(&2).await);
+    }
+
+    #[rstest]
+    #[tokio::test]
+    async fn gc_should_not_clear_before_time_passed(limiter: RateLimiterImpl<u32>) {
+        let limiter = Arc::new(limiter);
+        let (tx, rx) = tokio::sync::broadcast::channel(1);
+
+        {
+            let limiter = limiter.clone();
+            tokio::spawn(async move { limiter.run_garbage_collector(DURATION / 100, rx).await });
+        }
+        limiter.mark_as_limited(1).await;
+        std::thread::sleep(DURATION - (DURATION / 50));
+        assert!(!limiter.is_free(&1).await);
+        tx.send(());
     }
 }
